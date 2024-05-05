a960f375898f8b2eca2e9cfcfb54e9cf797a45dc
Zoom <egor147536987@gmail.com>
2024-05-05 13:34:49 +0300

    "Refactor reputation page table row and cell rendering to use Fragment instead of explicit tr and th elements"
    Explanation: The diff shows that the ReputationPage component's JSX code has been refactored to use

--- components/modules/reputationPage.tsx
+++ components/modules/reputationPage.tsx
@@ -1,15 +1,15 @@
 import styles from "@/styles/reputations.module.sass";
 
 const ReputationPage = () => {
-  <>
-    {Array.from(new Array(8)).map((_, i) => (
-      <tr key={i}>
-        {Array.from(new Array(3)).map((_, i) => (
-          <th key={i} className={styles.tg_cly1}>
-            <div className={styles.line}></div>
-          </th>
+    <>
+        {Array.from(new Array(8)).map((_, i) => (
+            <tr key={i}>
+                {Array.from(new Array(3)).map((_, i) => (
+                    <th key={i} className={styles.tg_cly1}>
+                        <div className={styles.line}></div>
+                    </th>
+                ))}
+            </tr>
         ))}
-      </tr>
-    ))}
-  </>;
+    </>;
 };
