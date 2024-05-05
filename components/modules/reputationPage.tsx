f048fb27f24707aee10823f832950ba745ab1ad7
Zoom <egor147536987@gmail.com>
2024-05-05 13:34:12 +0300

    Update table header cells in ReputationPage component
    * Replace empty div with a div containing the "line" class in each table header cell
    * Add "tg\_cly1" class to each table header cell
    * The

--- components/modules/reputationPage.tsx
+++ components/modules/reputationPage.tsx
@@ -5,8 +5,8 @@ const ReputationPage = () => {
     {Array.from(new Array(8)).map((_, i) => (
       <tr key={i}>
         {Array.from(new Array(3)).map((_, i) => (
-          <th key={i}>
-            <div className=""></div>
+          <th key={i} className={styles.tg_cly1}>
+            <div className={styles.line}></div>
           </th>
         ))}
       </tr>
