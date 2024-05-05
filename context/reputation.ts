4637e2c664575cf2b686eec5d7c3ff7ebfeea734
Zoom <egor147536987@gmail.com>
2024-05-05 14:35:58 +0300

    "Change $reputation type to IReputation[] in reputation.ts"
    Explanation:
    
    - The `a/context/reputation.ts` file had `$reputation` as a store

--- context/reputation.ts
+++ context/reputation.ts
@@ -1,6 +1,7 @@
+import { IReputation } from "@/types/reputation";
 import { createDomain } from "effector";
 
 
 const reputation = createDomain()
 
-export const $reputation = reputation.createStore<IReputation>
+export const $reputation = reputation.createStore<IReputation[]>([])
