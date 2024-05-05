535f9ea92a3281b0797ac2b920bd1b72e415baa3
Zoom <egor147536987@gmail.com>
2024-05-05 14:50:33 +0300

    "Replace reputation styles file extension from sass to scss and wrap ReputationSkeleton in a fragment"
    The changes include:
    - Renaming the file extension of "@/styles/reputations.module.sass

--- components/templates/ReputationPage.tsx
+++ components/templates/ReputationPage.tsx
@@ -3,26 +3,43 @@
 import { $reputation, ReputationGate } from "@/context/reputation";
 import { useGate, useStore } from "effector-react";
 import { getReputationsFx } from "../../api/reputation";
+import ReputationSkeleton from "../modules/reputationSkeleton";
+import styles from "@/styles/reputations.module.sass";
+
 
 export default function ReputationPage() {
   useGate(ReputationGate);
   const reputations = useStore($reputation);
   const spinner = useStore(getReputationsFx.pending);
-  return <main>
-    <section>
+  return (
+    <main>
+      <section>
         <div className="container">
-            <div>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Место</th>
-                            <th>Репутация</th>
-                            <th>Участник</th>
-                        </tr>
-                    </thead>
-                </table>
-            </div>
+          <div>
+            <table>
+              <thead>
+                <tr>
+                  <th>Место</th>
+                  <th>Репутация</th>
+                  <th>Участник</th>
+                </tr>
+              </thead>
+              <tbody>
+                {spinner ? (
+                  <ReputationSkeleton />
+                ) : (
+                  reputations.map(({reputation, fullName, userAvatar}, index) => (
+                    <tr key={index}>
+                      <th>{index + 1}</th>
+                      <td className={styles.full_name}></td>
+                    </tr>
+                  ))
+                )}
+              </tbody>
+            </table>
+          </div>
         </div>
-    </section>
-  </main>
+      </section>
+    </main>
+  );
 }
