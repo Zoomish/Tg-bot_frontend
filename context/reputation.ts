33d8ec81f8a3a03fcbea0d5d476e2f72021b0bae
Zoom <egor147536987@gmail.com>
2024-05-05 14:37:16 +0300

    "Add getReputationsFx to reputation store and handle its done effect"
    This commit adds the `getReputationsFx` effect to the `reputation` store and handles its `done` effect by updating the

--- context/reputation.ts
+++ context/reputation.ts
@@ -1,7 +1,9 @@
+import { getReputationsFx } from "@/api/reputation";
 import { IReputation } from "@/types/reputation";
 import { createDomain } from "effector";
 
+const reputation = createDomain();
 
-const reputation = createDomain()
-
-export const $reputation = reputation.createStore<IReputation[]>([])
+export const $reputation = reputation
+  .createStore<IReputation[]>([])
+  .on(getReputationsFx.done, (_, { result }) => result);
