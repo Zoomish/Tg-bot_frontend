3799dbfffef0720cf8b136e2341146df88345bdb
Zoom <egor147536987@gmail.com>
2024-05-05 13:30:54 +0300

    "Add empty table headers to ReputationPage"
    This commit adds an empty table row with three table headers to the ReputationPage component. Each header contains an empty div.

--- components/modules/reputationPage.tsx
+++ components/modules/reputationPage.tsx
@@ -1,7 +1,13 @@
 const ReputationPage = () => {
   <>
     {Array.from(new Array(8)).map((_, i) => (
-      <tr key={i}></tr>
+      <tr key={i}>
+        {Array.from(new Array(3)).map((_, i) => (
+          <th key={i}>
+            <div className=""></div>
+          </th>
+        ))}
+      </tr>
     ))}
   </>;
 };
