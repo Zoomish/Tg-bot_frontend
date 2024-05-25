d16c240b08bd285236f93c4cc07292e52ed397dd
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-05-25 14:48:57 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import ReputationPage from "@/components/templates/ReputationPage";
 export const metadata = {
   title: "Репутация участников",
 };
+
 export default function Reputation() {
   return <ReputationPage></ReputationPage>;
 }
